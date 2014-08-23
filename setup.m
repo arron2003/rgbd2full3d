1a827e603b9fc2f0c1d958ed1fef69856c4daaee
arron2003 <arron2003@gmail.com>
2014-08-23 10:34:40 -0500

    Adding config

--- setup.m
+++ setup.m
@@ -7,6 +7,10 @@ if ~exist('mat')
   !wget -r -l1 -np "http://aqua.cs.uiuc.edu/mat" -P . -A "*.mat" --no-host-directories --no-verbose
 end
 
+if ~exist('config')
+  !wget -r -l1 -np "http://aqua.cs.uiuc.edu/config" -P . -A "*.mat" --no-host-directories --no-verbose
+end
+
 !mkdir cache
 !mkdir cache_gt
 !mkdir cache_prop
